83e4fa0f5a0ac615632dc653637b48c2f07b9246
Aaron <138522118+Akaina1@users.noreply.github.com>
2024-06-27 18:26:08 +0000

    integrate docker container - working

--- akaina/app/api/[[...slugs]]/route.ts
+++ akaina/app/api/[[...slugs]]/route.ts
@@ -1,13 +1,16 @@
-// app/[[...slugs]]/route.ts
+// app/api/[[...slugs]]/route.ts
 import { Elysia, t } from 'elysia'
 
 const app = new Elysia({ prefix: '/api' })
-    .get('/', () => 'hello Next - From Elysia')
+    .get('/', () => 'hello Next - From Elysia 13.0.0')
     .post('/', ({ body }) => body, {
         body: t.Object({
             name: t.String()
         })
     })
 
-export const GET = app.handle 
-export const POST = app.handle 
+// Compile the Elysia application
+app.compile();
+
+export const GET = app.handle
+export const POST = app.handle
