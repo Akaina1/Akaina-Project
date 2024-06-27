2a6aec4533cb140ad60ed8672ae395a547aacabe
Aaron <138522118+Akaina1@users.noreply.github.com>
2024-06-27 14:15:45 +0000

    Next and Elysia Setup

---
+++ akaina/app/api/[[...slugs]]/route.ts
@@ -0,0 +1,13 @@
+// app/[[...slugs]]/route.ts
+import { Elysia, t } from 'elysia'
+
+const app = new Elysia({ prefix: '/api' })
+    .get('/', () => 'hello Next - From Elysia')
+    .post('/', ({ body }) => body, {
+        body: t.Object({
+            name: t.String()
+        })
+    })
+
+export const GET = app.handle 
+export const POST = app.handle 
